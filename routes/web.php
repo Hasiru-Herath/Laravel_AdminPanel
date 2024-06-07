a7d458c650391d4c4531077c284e9de7b7cb1419
Hasiru-Herath <hasiruherath.13@gmail.com>
2024-06-07 11:45:49 +0530

    initial_files

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-// uuse App\Http\Controllers\AdminController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,21 +15,3 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
-
-Route::group([
-    'prefix' => 'categories'
-  ], function() {
-    Route::get('/', [CategoryController::class, 'index']);
-
-    Route::get('/create', [CategoryController::class, 'create'])->name('create-category');
-    Route::post('/store', [CategoryController::class, 'store'])->name('store-category');
-
-     
-    Route::get('/edit/{id}', [CategoryController::class, 'edit']);
-    Route::post('/update/{id}', [CategoryController::class, 'update']);
-
-    Route::get('/delete/{id}', [CategoryController::class, 'destroy']);
-
-  
-  });
-
